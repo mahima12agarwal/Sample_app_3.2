b7695d0579d6293ac65a4a47b552ca8ff048bcd2
Mahima Agarwal <mahima.agarwal@accenture.com>
2012-07-13 12:04:37 +0530

    final commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.2'
+
+gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
+
+group :development do
+  gem 'sqlite3', '1.3.5'
+end
+
+# Asset template engines
+group :assets do
+  gem 'sass-rails', "~> 3.2.4"
+  gem 'coffee-rails', '~> 3.2.2'
+  gem 'uglifier', '>= 1.2.3'
+end
+
+gem 'jquery-rails', '2.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp31::Application.config.secret_token = 'a1aa829c6a0b481e376926926be18b3f3cf1fb47cc722d7adc37f4f95eaa8adee8d6637e5bea9fcec728a28534e5e82b86df258456a63927e02472d9c6aced2e'
